260f31f274af1be9b97ae0d210c19986638accf6
Immanuella Umoren <immanuellaumoren@gmail.com>
2024-03-25 23:37:54 -0500

    Required and Stretch features Done

--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -6,20 +6,67 @@
 import UIKit
 import Nuke
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("🍏 numberOfRowsInSection called with movies count: \(posts.count)")
+        return posts.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        
+        let post = posts[indexPath.row]
+        
+        if let photo = post.photos.first {
+            let url = photo.originalSize.url
+                  
+                Nuke.loadImage(with: url, into: cell.postImageView)
+            }
 
+            cell.summaryLabel.text = post.summary
 
+            return cell
+        
+        
+    }
+    
+    private let refreshControl = UIRefreshControl()
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var posts: [Post] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        if #available(iOS 10.0, *) {
+               tableView.refreshControl = refreshControl
+           } else {
+               tableView.addSubview(refreshControl)
+           }
+
+           refreshControl.addTarget(self, action: #selector(refreshData(_:)), for: .valueChanged)
+           
 
         
         fetchPosts()
     }
 
+    @objc private func refreshData(_ sender: Any) {
+        print("🔄 Starting to refresh data...")
+        
+        refreshControl.beginRefreshing()
+        fetchPosts()
+    }
 
 
     func fetchPosts() {
-        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
+        let url = URL(string: "https://api.tumblr.com/v2/blog/nationalgeographicmagazine.tumblr.com/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
         let session = URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 print("❌ Error: \(error.localizedDescription)")
@@ -48,6 +95,11 @@ class ViewController: UIViewController {
                     for post in posts {
                         print("🍏 Summary: \(post.summary)")
                     }
+                    
+                    self?.posts = posts
+                    self?.tableView.reloadData()
+                    self?.refreshControl.endRefreshing()
+                    print("🍏 Fetched and stored \(posts.count) movies")
                 }
 
             } catch {
